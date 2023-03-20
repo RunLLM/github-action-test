e1f50665ad23fec8e74a245c6e518c5383fa490e
cw75 <cgwu0530@gmail.com>
2023-03-20 21:32:24 +0000

    test

---
+++ workflow/test.py
@@ -0,0 +1,12 @@
+from aqueduct import Client, op
+import os
+
+client = Client(aqueduct_address=os.environ['AQUEDUCT_SERVER_ADDRESS'], api_key=os.environ['AQUEDUCT_API_KEY'])
+
+@op
+def test():
+    return "hello"
+
+result = test()
+
+client.publish_flow(name="test", artifacts=[result])
